35cb19f8e92511edd75d661732b9402c5dadbbd5
SH0315 <lsh030315@naver.com>
2026-05-06 19:13:38 +0900

    feat: 로그인시 JDBC 연동 추가

--- SangHyeok/StudyServlet/src/main/java/com/dongyang/studyservlet/LoginServlet.java
+++ SangHyeok/StudyServlet/src/main/java/com/dongyang/studyservlet/LoginServlet.java
@@ -26,8 +26,15 @@ public class LoginServlet extends HttpServlet {
         String id = request.getParameter(  "id");
         String pw = request.getParameter(  "pw");
 
+        MemberDTO mdto = new MemberDTO();
+        mdto.setMemberid(id);
+        mdto.setPassword(pw);
+
+        MemberDAO mdao = new MemberDAO();
+        boolean result = mdao.loginCheck(mdto);
+
         // 2. JDBC
-        if(id.equals("dong") && pw.equals("123")) {
+        if(result) {
             // 조건 충족시 (성공)
             HttpSession session = request.getSession();
             session.setAttribute("userName", "김동양");
