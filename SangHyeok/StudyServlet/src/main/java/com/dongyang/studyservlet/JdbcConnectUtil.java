f0505f0f0bd61c24f606d8afce0952c7c694f3ab
Lim_SangHyeok <130148600+SH0315@users.noreply.github.com>
2026-05-23 17:38:21 +0900

    [WEEK04] 임상혁 4주차 실습 (#14)

---
+++ SangHyeok/StudyServlet/src/main/java/com/dongyang/studyservlet/JdbcConnectUtil.java
@@ -0,0 +1,39 @@
+package com.dongyang.studyservlet;
+
+import java.sql.*;
+
+public class JdbcConnectUtil {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/servletdb", "root", "0000");
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(Connection conn, PreparedStatement pstmt){
+        try {
+            conn.close();
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void close(Connection conn, PreparedStatement pstmt, ResultSet rs){
+        try {
+            conn.close();
+            pstmt.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
