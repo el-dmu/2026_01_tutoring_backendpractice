8ca55143c256bc3517688b850d46daec6160e8aa
DavidPark04 <deasoon4919@gmail.com>
2026-06-06 02:30:50 +0900

    feat:Filter 추가

---
+++ Dawit/StudyServlet/src/main/java/com/dongyang/studyservlet/FilterServlet.java
@@ -0,0 +1,28 @@
+package com.dongyang.studyservlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/filter.do")
+public class FilterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		request.setAttribute("filterName", name);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("filterResult.jsp");
+		dispatcher.forward(request, response);
+		return;
+	}
+
+}
